57425f12e91c9b62cc43d4fe06da89065ee184ea
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-02-01 19:58:53 +0000

    Create palindrome-number.py

---
+++ palindrome-number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        x = str(x)
+
+        l = 0
+        r = len(x)-1
+
+        while l <= r:
+            if(x[l] != x[r]): return False
+            l+=1
+            r-=1
+
+        return True
+            
+        
