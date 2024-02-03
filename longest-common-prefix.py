cafd8e697f48029b0d63887b3097a176130ee1ba
e.hokage777 <e.hokage777@gmail.com>
2024-02-03 17:00:07 +0000

    completed: longest-common-prefix

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -8,27 +8,20 @@ class Solution(object):
         ## case where only 1 string in list
         if len(strs) <= 1:
             return strs[0]
-        elif len(strs[0]) == 0:
-            return ""
         
         index = 0
         end = False
         for char in strs[0]:
             for i in range(1, len(strs)):
-                if len(strs[i])== 0:
-                    end = True
-                    index = 0
-                    break
-                elif len(strs[i])-1 <  index or strs[i][index] != char:
-                    print("char",char)
+                if len(strs[i])-1 <  index or strs[i][index] != char:
                     end = True
                     break
 
             index += 1
 
-            if end: break
+            if end: return strs[0][:index-1]
 
-        return strs[0][:index-1]
+        return strs[0]
 
 solution = Solution()
 
