c0fe17f131974d2d29083ecbabf1be470168ee19
e.hokage777 <e.hokage777@gmail.com>
2024-02-09 12:11:16 +0000

    completed list-comprehension

---
+++ list-comprehension.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    print([[xi,yi,zi] for xi in range(x+1) for yi in range(y+1) for zi in range(z+1) if(xi+yi+zi) != n])
