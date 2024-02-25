1aa9a86e7b3062c554b8e4215d33bef045561f92
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-02-25 15:28:16 +0000

    Time: 101 ms (95.12%), Space: 12.9 MB (72.36%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -8,10 +8,10 @@ class Solution(object):
         del_col_count = 0
 
         for j in range(len(strs[0])):
-            current = ord(strs[0][j])
+            current = strs[0][j]
             for i in range(1,len(strs)):
-                if ord(strs[i][j]) >= current:
-                    current = ord(strs[i][j])
+                if strs[i][j] >= current:
+                    current = strs[i][j]
                 else:
                     del_col_count += 1
                     break
