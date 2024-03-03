7eefb7a56b48fc4f464da3e23691c580ede71fa7
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-03-03 22:25:57 +0000

    Time: 22 ms (40.45%), Space: 11.6 MB (66.29%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -6,20 +6,32 @@ class Solution(object):
         :rtype: int
         """
 
-        eliminated = set()
-        i = 0
-        j = k
-        while(len(eliminated) < n-1):
-            if i not in eliminated:
-                j -= 1
+        persons = [i+1 for i in range(n)]
+        index = 0
+        while len(persons) > 1:
+            index = (index + k-1)%n
+            persons.pop(index)
+            n = len(persons)
 
-            if j <= 0:
-                eliminated.add(i)
-                j = k
-
-            i = (i+1)%n
+        return persons[0]
 
 
-        for m in range(n):
-            if m not in eliminated:
-                return m+1
+
+
+        # eliminated = set()
+        # i = 0
+        # j = k
+        # while(len(eliminated) < n-1):
+        #     if i not in eliminated:
+        #         j -= 1
+
+        #     if j <= 0:
+        #         eliminated.add(i)
+        #         j = k
+
+        #     i = (i+1)%n
+
+
+        # for m in range(n):
+        #     if m not in eliminated:
+        #         return m+1
