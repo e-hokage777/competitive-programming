ce278a310e899f1d0459a688b7f394b4bd374fa4
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-03-28 19:43:30 +0000

    Time: 555 ms (31.07%), Space: 24.4 MB (95.02%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,17 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max_ind = 0
-        _min = 0
+
+        _min = min(0, nums[0])
+        max_sum = nums[0]
         for i in range(1, len(nums)):
             nums[i] = nums[i] + nums[i-1]
-            if nums[i] > nums[max_ind]:
-                max_ind = i
+            max_sum = max(max_sum, nums[i] - _min)
 
-        for i in range(max_ind):
             if nums[i] < _min:
                 _min = nums[i]
 
-        return nums[max_ind] - _min
+        return max_sum
+
+
+
 
         
