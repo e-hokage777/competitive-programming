10d13baed20722ce1b5c14ebd816fc330b8c56bf
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-04-17 20:02:36 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        tortoise = head
+        hare = head
+
+        if head.next:
+            hare = hare.next.next
+            tortoise = head.next
+        else:
+            return None
+
+        while hare and tortoise != hare:
+            if hare.next:
+                tortoise = tortoise.next
+                hare = hare.next.next
+            else:
+                return None
+
+
+        if not hare:
+            return None
+
+        
+        hare = head
+
+        while tortoise != hare:
+            tortoise = tortoise.next
+            hare = hare.next
+
+        return hare
+        
