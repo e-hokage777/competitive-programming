b5fc21a5fce46b9d26cad4fdb17a3365da122e61
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-04-19 18:34:56 +0000

    Time: 40 ms (12.45%), Space: 13.7 MB (6.36%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,19 @@
+import copy
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        
+        return copy.deepcopy(head)
+        
