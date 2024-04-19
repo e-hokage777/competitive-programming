2b05feec922732b5cf577fe97ceed818a1fb6bc7
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-04-19 19:59:35 +0000

    Time: 541 ms (29.17%), Space: 50.1 MB (16.67%) - LeetHub

--- 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.py
+++ 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.py
@@ -17,6 +17,8 @@ class Solution(object):
 
         holder = {0:-1}
 
+        if rem == 0: return n*(target//_sum)
+
         for i in range(len(nums)):
             diff = nums[i] - rem
 
@@ -28,71 +30,6 @@ class Solution(object):
         if min_length == float("inf"): return -1
 
         return min_length + n*(target//_sum)
-
-        
-
-        # _sum = 0
-        # holder = {0:1}
-        # i = 0
-        # min_length = 10e5
-        # while i<10e5:
-        #     _sum+=nums[i%len(nums)]
-        #     if (_sum - target) in holder:
-        #         min_length = min(min_length, i-holder[_sum-target])
-
-        #     holder[_sum] = i 
-
-        #     i+=1
-
-        # if min_length == 10e5:
-        #     return - 1
-        # return min_length
-
-        # prefix = []
-        # holder = {0:1}
-        # for i in range(len(nums)*2):
-        #     prefix.append(prefix[-1]+nums[i%len(nums)])
-        #     if
-            
-
-        # print(prefix)
-        # return 2
-
-        # prefix = []
-        # suffix = []
-        # holder = {0:0}
-        # min_length = float("inf")
-        # for i in range(len(nums)):
-        #     if i == 0:
-        #         prefix.append(nums[i])
-        #         suffix.append(nums[-i-1])
-        #     else:
-        #         prefix.append(prefix[-1] + nums[i])
-        #         suffix.append(suffix[-1] + nums[i])
-
-        #     if prefix[i]-target in holder:
-        #         min_length =  min(min_length, i+1 - holder[prefix[i]-target])
-
-        #     holder[prefix[i]] = i+1
-
-
-        # k = target/prefix[-1]
-        # remainder = target - k*prefix[-1]
-
-        # if remainder == 0:
-        #     min_length = min(min_length, k*len(nums))
-
-        # for i in range(len(suffix)):
-        #     if remainder-suffix[i] in holder:
-        #         print(holder)
-        #         min_length = min(min_length, i + holder[remainder-suffix[i]]+1 + k*prefix[-1])
-
-        # # return min_length
-
-        # if min_length == float("inf"):
-        #     return -1
-        
-        # return min_length
 
 
         
