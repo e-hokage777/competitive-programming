029c56179856f0ea73832a45f89598730b3b97ec
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-05-21 18:28:52 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -0,0 +1,79 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray(object):
+#    def get(self, index):
+#        """
+#        :type index: int
+#        :rtype int
+#        """
+#
+#    def length(self):
+#        """
+#        :rtype int
+#        """
+
+class Solution(object):
+    def findInMountainArray(self, target, mountain_arr):
+        """
+        :type target: integer
+        :type mountain_arr: MountainArray
+        :rtype: integer
+        """
+        
+        ## finding peak
+        left = -1
+        right = mountain_arr.length()
+
+        while left < right:
+            middle = left + (right-left)//2
+
+            if mountain_arr.get(middle) > mountain_arr.get(middle+1):
+                right = middle
+            else:
+                left = middle + 1
+
+
+        ## search for taret in first half
+        res = self.find_target(0, right, mountain_arr, target)
+
+        if res != -1:
+            return res
+
+        ## search right target
+        res = self.find_target_reverse(right, mountain_arr.length()-1, mountain_arr, target)
+
+        return res
+
+        
+
+    def find_target(self, left, right, mountain_arr, target):
+        
+        while left <= right:
+            middle = left + (right - left)//2
+
+            value = mountain_arr.get(middle)
+            if value == target:
+                return middle
+            elif target > value:
+                left = middle + 1
+            else:
+                right = middle - 1
+
+        return -1
+
+    def find_target_reverse(self, left, right, mountain_arr, target):
+        
+        while left <= right:
+            middle = left + (right - left)//2
+
+            value = mountain_arr.get(middle)
+            if value == target:
+                return middle
+            elif target < value:
+                left = middle + 1
+            else:
+                right = middle - 1
+
+        return -1
