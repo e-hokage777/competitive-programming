3bacb132dfbc9718cc2ae3923752143e940ae0cc
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-08-13 18:22:57 +0000

    Time: 13 ms (75.48%), Space: 11.7 MB (29.39%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,14 +1,22 @@
 class Solution(object):
+    def __init__(self):
+        self.memo = dict()
+
     def fib(self, n):
         """
         :type n: int
         :rtype: int
         """
 
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
+        if n == 0: return 0
+        if n == 1: return 1
+        
+        dp = [0]*(n+1)
 
-        return self.fib(n-1) + self.fib(n-2)
+        dp[0],dp[1] = 0, 1
+
+        for index in range(2, n+1):
+            dp[index] = dp[index-1] + dp[index-2]
+
+        return dp[n]
         
