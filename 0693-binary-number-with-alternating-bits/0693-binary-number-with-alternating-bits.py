69e573638613ef7ffe3eb59aa617100776482eb1
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-08-29 19:37:23 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
@@ -5,12 +5,5 @@ class Solution(object):
         :rtype: bool
         """
 
-        checker = bool(n&1)
-
-        while n:
-            if checker != bool(n&1): return False
-            n>>=1
-            checker = not checker
-
-        return True
+        return not n & (n>>1)
         
