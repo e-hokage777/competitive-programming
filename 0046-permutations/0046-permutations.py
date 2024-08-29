b8919fb21d30a8d0d3acd385ee3733ca600542b4
e-hokage777 <81178148+e-hokage777@users.noreply.github.com>
2024-08-29 17:58:12 +0000

    Time: 23 ms (40.44%), Space: 12.1 MB (14.13%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -5,19 +5,21 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
-        self.result = set()
-        def backtrack(index):
-            if index >= len(nums):
-                return
+        max_num = 2**len(nums)-1
+        result = []
+        cur = []
+        def backtrack(index, current):
+            if current == max_num:
+                result.append(cur[:])
 
-            for i in range(index, len(nums)):
-                nums[index], nums[i] = nums[i], nums[index]
-                backtrack(index+1)
-                self.result.add(tuple(nums[:]))
-                nums[i], nums[index] = nums[index], nums[i]
+            for i in range(len(nums)):
+                if not current & 2**i:
+                    cur.append(nums[i])
+                    backtrack(index+1, current|2**i)
+                    cur.pop()
 
 
-        backtrack(0)
+        backtrack(0, 0)
 
-        return list(self.result)
+        return result
         
