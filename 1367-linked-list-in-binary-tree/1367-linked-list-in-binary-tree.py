4743df800c067bbfd917c0f9a03162e7d297ca84
Eli Bansa <81178148+e-hokage777@users.noreply.github.com>
2024-12-15 20:36:45 +0000

    Time: 28 ms (57.14%), Space: 13.7 MB (77.95%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
@@ -17,18 +17,45 @@ class Solution(object):
         :rtype: bool
         """
 
-        def dfs(treenode, listnode):
-            if not listnode:
-                return True
-            if not treenode:
-                return False
+        # def dfs(treenode, listnode):
+        #     if not listnode:
+        #         return True
+        #     if not treenode:
+        #         return False
 
-            if treenode.val != listnode.val:
-                listnode = head if head.val != treenode.val else head.next
-                return dfs(treenode.left, listnode) or dfs(treenode.right, listnode)
-            else:
-                return dfs(treenode.left, listnode.next) or dfs(treenode.right, listnode.next)
+        #     if treenode.val != listnode.val:
+        #         listnode = head if head.val != treenode.val else head.next
+        #         return dfs(treenode.left, listnode) or dfs(treenode.right, listnode)
+        #     else:
+        #         return dfs(treenode.left, listnode.next) or dfs(treenode.right, listnode.next)
 
         
-        return dfs(root, head)
+        # return dfs(root, head)
+
+        stack = [root]
+
+        while stack:
+            current = stack.pop()
+
+            if self.has_valid_path(current, head):
+                return True
+            
+            if current.left:
+                stack.append(current.left)
+            if current.right:
+                stack.append(current.right)
+
+        return False
+
+
+    def has_valid_path(self, treenode, listnode):
+        if not listnode:
+            return True
+        if not treenode:
+            return False
+
+        if treenode.val != listnode.val:
+            return False
+        else:
+            return self.has_valid_path(treenode.left, listnode.next) or self.has_valid_path(treenode.right, listnode.next)
         
