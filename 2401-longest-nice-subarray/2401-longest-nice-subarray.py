a9d7b213f3c6e66cb41d071400ffd15310506663
Eli Bansa <81178148+e-hokage777@users.noreply.github.com>
2025-07-17 12:48:57 +0000

    Time: 215 ms (7.27%), Space: 21.8 MB (45.45%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -4,58 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-        ans = 1
+        max_length = 1
         for i in range(len(nums)):
-            holder = 0
-            count = 0
-            for j in range(i, min(i+30, len(nums))):
-                holder = self.test(holder, nums[j])
-                if holder  == -1:
+            holder = nums[i]
+            length = 1
+            for j in range(i+1, min(i+30, len(nums))):
+                if holder & nums[j]:
                     break
-        
-                count += 1
-            
-            ans = max(ans, count)
+                
+                holder |= nums[j]
 
-        return ans
-                    
+                length += 1
+                max_length = max(length, max_length)
 
-    def test(self, a, b):
-        count_extra = True
-        extra = 0
-        ans = 0
-        while a > 0 or b > 0:
-            ans <<= 1
-            if (a & b) & 1:
-                return -1
-            elif (a & 1) or (b & 1):
-                ans |= 1
-                count_extra = False
-            else:
-                if count_extra:
-                    extra += 1
-
-            a >>= 1
-            b >>= 1
-
-
-
-        actual_ans = 0
-
-        while ans > 0:
-            actual_ans <<= 1
-            actual_ans |= (ans & 1)
-            ans >>= 1
-
-        while extra > 0:
-            actual_ans <<= 1
-            extra -= 1
-
-        return actual_ans
-            
-
-            
-
+        return max_length
+                
                 
                     
